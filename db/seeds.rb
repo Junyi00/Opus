761494c34706dca0c64c64e8170770541eef74a6
Goh Jun Yi <gohjunyi00@gmail.com>
2021-12-12 18:07:43 +0800

    Sample Seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,65 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([
+    {
+        username: "develop",
+        password_digest: BCrypt::Password.create("develop"),
+        email: "dev@dev.com"
+    }
+])
+
+projects = Project.create([
+    {
+        name: "Study",
+        user: users.first
+    }
+])
+
+lanes = Lane.create([
+    {
+        name: "New",
+        project: projects.first
+    },
+    {
+        name: "In Progress",
+        project: projects.first
+    },
+    {
+        name: "Completed",
+        project: projects.first
+    }
+])
+
+tasks = Task.create([
+    {
+        name: "Draw Mindmap",
+        description: "MODULE 1",
+        starred: false,
+        lane: Lane.find_or_create_by(name: "New")
+    },
+    {
+        name: "Cheatsheet",
+        description: "MODULE 1",
+        starred: true,
+        lane: Lane.find_or_create_by(name: "In Progress")
+    },
+    {
+        name: "PYP",
+        description: "MODULE 1",
+        starred: true,
+        lane: Lane.find_or_create_by(name: "Completed")
+    }
+])
+
+tags = Tag.create([
+    {
+        name: "Exam",
+        task: Task.find_or_create_by(name: "PYP")
+    },
+    {
+        name: "Notes",
+        task: Task.find_or_create_by(name: "Draw Mindmap")
+    }
+])
