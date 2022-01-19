27dd460c2805c2e516f8a1d451b5cea9a0dbed8f
Goh Jun Yi <gohjunyi00@gmail.com>
2022-01-20 02:12:59 +0800

    Self-hosting and utilising Open Sans Font

--- src/config/application.rb
+++ src/config/application.rb
@@ -18,5 +18,8 @@ module Opus
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    # compile fonts
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
